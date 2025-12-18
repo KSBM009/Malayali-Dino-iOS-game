7c10d2628efb341b89797a94a555d27a78be8a22
KSBM009 <kevin@emp.ogesinfotech.com>
2025-12-18 17:48:01 +0530

    Created a New Project and got everything set ready.

---
+++ MalayaliDinoGame/MalayaliDinoGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MalayaliDinoGame
+//
+//  Created by macbook on 18/12/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
